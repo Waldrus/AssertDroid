5a76b1aec962ddfbde8762180c676a599bc8ec1f
Vladimir Ilyin <viljin@list.ru>
2014-05-23 09:56:31 +0300

    Получение конструктора

--- library/src/java/ru/waldrus/assertdroid/AssertDroid.java
+++ library/src/java/ru/waldrus/assertdroid/AssertDroid.java
@@ -18,6 +18,10 @@ public class AssertDroid extends Assert {
     public static AssertDroidConfig init(boolean debug){
         return new AssertDroidConfig(debug);
     }
+    
+    public static AssertDroidConfig getConfig(){
+        return new AssertDroidConfig();
+    }
 
     public static AssertActivity assertThat(Activity activity){
         return AssertActivity.assertThat(activity);
