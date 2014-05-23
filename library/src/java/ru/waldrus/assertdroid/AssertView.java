7537ea7035ad37a125f281ddde343040230c69b6
Vladimir Ilyin <viljin@list.ru>
2014-05-23 10:33:39 +0300

    Добавил методов проверки вью

--- library/src/java/ru/waldrus/assertdroid/AssertView.java
+++ library/src/java/ru/waldrus/assertdroid/AssertView.java
@@ -1,6 +1,8 @@
 package ru.waldrus.assertdroid;
 
 import android.view.View;
+import android.view.ViewGroup;
+
 
 /**
  * Created by Wald on 11.04.2014.
@@ -13,5 +15,59 @@ public class AssertView extends AssertComponent<AssertView, View> {
 
     public static AssertView assertThat(View view){
         return new AssertView(view);
+    }
+    
+    public AssertView assertEnabled(){
+        notNull();
+        Assert.assertTrue(mObject.isEnabled());
+        return true;
+    }
+    
+    public AssertView assertDisabled(){
+        notNull();
+        Assert.assertFalse(mObject.isEnabled());
+        return true;
+    }
+    
+    public AssertView assertVisible(){
+        notNull();
+        Assert.assertTrue(mObject.getVisibility() == View.VISIBLE);
+        return this;
+    }
+    
+    public AssertView assertInvisible(){
+        notNull();
+        Assert.assertTrue(mObject.getVisibility() == View.INVISIBLE);
+        return this;
+    }
+    
+    public AssertView assertGone(){
+        notNull();
+        Assert.assertTrue(mObject.getVisibility() == View.GONE);
+        return this;
+    }
+    
+    public AssertView assertNotVisible(){
+        notNull();
+        Assert.assertTrue(mObject.getVisibility() != View.VISIBLE);
+        return this;
+    }
+    
+    public AssertView assertIsLayout(){
+        notNull();
+        Assert.assertTrue(mObject instanceof ViewGroup);
+        return this;
+    }
+    
+    public AssertView assertNotLayout(){
+        notNull();
+        Assert.assertFalse(mObject instanceof ViewGroup);
+        return this;
+    }
+    
+    public AssertView assertContainsChild(int count){
+        assertIsLayout();
+        Assert.assertTrue(((ViewGroup)mObject).getChildCount() > 0);
+        return this;
     }
 }
